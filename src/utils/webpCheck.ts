2d6f93cad2efb821f2d421d60d4d7c5e8d4cdb5a
phytocodes <230316458+phytocodes@users.noreply.github.com>
2025-12-01 10:44:14 +0900

    webpCheck 追加

---
+++ src/utils/webpCheck.ts
@@ -0,0 +1,23 @@
+export function  webpCheck(classNameToApply: string = 'no-webp'): void {
+
+	const webpTestImage: string = 'data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==';
+	const img = new Image();
+	const body = document.body;
+
+	img.onload = (): void => {
+		if (img.width > 0 && img.height > 0) {
+			// body.classList.add('webp');
+			console.log('WebP: Supported');
+		} else {
+			body.classList.add(classNameToApply);
+			console.log(`WebP: Unsupported (loaded but zero size). Class added: ${classNameToApply}`);
+		}
+	};
+
+	img.onerror = (): void => {
+		body.classList.add(classNameToApply);
+		console.log(`WebP: Unsupported (load error). Class added: ${classNameToApply}`);
+	};
+
+	img.src = webpTestImage;
+}
